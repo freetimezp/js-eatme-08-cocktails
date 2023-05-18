bc509e213d50e65dea1816176b33eb405bb14fc6
freetimezp <kraftukrnet@ukr.net>
2023-05-18 22:57:51 +0300

    02. js-eatme-08-cocktails. Router.

---
+++ src/pages/CocktailRandom.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        random
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="sass" scoped>
+@import '../assets/styles/main'
+</style>
