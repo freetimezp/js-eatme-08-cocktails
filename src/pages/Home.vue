4aeaef1869352c1afb7a8276109604cdbca2a0cb
freetimezp <kraftukrnet@ukr.net>
2023-05-19 15:14:51 +0300

    03. js-eatme-08-cocktails. AppLayout Component.

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,14 +1,16 @@
-<template>
-    <div>
-        home
-    </div>
-</template>
-
 <script>
-export default {
+import AppLayout from '../components/AppLayout.vue'
 
+export default {
+    components: {
+        AppLayout
+    },
 }
 </script>
+
+<template>
+    <AppLayout imgUrl="/src/assets/img/bg-1.jpg" />
+</template>
 
 <style lang="sass" scoped>
 @import '../assets/styles/main'
